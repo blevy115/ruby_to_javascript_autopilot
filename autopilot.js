26a244050ae733eef2a278313c675a2f3002f796
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-18 16:03:55 -0500

    completed drive function

--- autopilot.js
+++ autopilot.js
@@ -13,3 +13,32 @@ function pickUpPassenger(car){
   car['gas'] -= 10;
   console.log('Picked up passenger. Car now has '+car['passengers']+' passengers.');
 }
+
+function getDestination(car){
+  if (car['city']==='Toronto'){
+    return 'Mississauga';
+  }else if (car['city']==='Mississauga'){
+    return 'London';
+  }else if (car['city']==='London'){
+    return 'Toronto'
+  }
+}
+
+function fillUpGas(car){
+  oldGas = car['gas'];
+  car['gas'] = 100;
+  return 'Filled up to ' + getGasDisplay(car['gas'])+' on gas from '+getGasDisplay(oldGas)+'.';
+}
+
+function getGasDisplay(gasAmount){
+  return gasAmount+'%';
+}
+
+function drive(car, cityDistance){
+  if(car['gas']<cityDistance){
+    return fillUpGas(car);
+  }
+  car['city'] = getDestination(car);
+  car['gas'] -= cityDistance;
+  return 'Drove to '+car['city']+'. Remaining gas: '+getGasDisplay(car['gas'])+'.';
+}
